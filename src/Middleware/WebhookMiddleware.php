3b9badf36bce504ed6a72dc8ee08def4a70a4c09
Stanislav Surskiy <ssurskiy@gmail.com>
2023-05-29 21:34:11 +0400

    fix: logging payload

--- src/Middleware/WebhookMiddleware.php
+++ src/Middleware/WebhookMiddleware.php
@@ -46,7 +46,7 @@ class WebhookMiddleware
     {
         try {
             $message = MessageFactory::create($request);
-            Log::debug('Event message', [ $message ]);
+            Log::debug('Event message', [ 'message' => $message ]);
             $request->merge([
                 'cdek_event_type' => $message->type
             ]);
