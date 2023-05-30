6455401d267716ea54f0d53540ce411e5482df33
Stanislav Surskiy <ssurskiy@gmail.com>
2023-05-30 10:29:44 +0400

    fix: Message DTOs

--- src/Messages/DownloadPhotoMessage.php
+++ src/Messages/DownloadPhotoMessage.php
@@ -3,12 +3,16 @@
 namespace Muscobytes\CdekWebhook\Messages;
 
 use Carbon\Carbon;
+use Muscobytes\CdekWebhook\Messages\Casts\TypeCast;
 use Muscobytes\CdekWebhook\Messages\Properties\DownloadPhotoAttributes;
 use Muscobytes\CdekWebhook\Messages\Properties\Type;
+use Spatie\LaravelData\Attributes\WithCast;
+use Spatie\LaravelData\Data;
 
-class DownloadPhotoMessage
+class DownloadPhotoMessage extends Data
 {
     public function __construct(
+        #[WithCast(TypeCast::class)]
         public Type $type,
 
         public Carbon $date_time,
--- src/Messages/PrealertClosedMessage.php
+++ src/Messages/PrealertClosedMessage.php
@@ -3,13 +3,16 @@
 namespace Muscobytes\CdekWebhook\Messages;
 
 use Carbon\Carbon;
+use Muscobytes\CdekWebhook\Messages\Casts\TypeCast;
 use Muscobytes\CdekWebhook\Messages\Properties\PrealertClosedAttributes;
 use Muscobytes\CdekWebhook\Messages\Properties\Type;
+use Spatie\LaravelData\Attributes\WithCast;
 use Spatie\LaravelData\Data;
 
 class PrealertClosedMessage extends Data
 {
     public function __construct(
+        #[WithCast(TypeCast::class)]
         public Type $type,
 
         public Carbon $date_time,
